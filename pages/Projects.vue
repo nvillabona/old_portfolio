a0171397628ecf12313a0ad758059b380d6784c6
Nicolas Villabona <nvillabona97@gmail.com>
2020-09-29 12:56:27 -0500

    added currency info

--- pages/Projects.vue
+++ pages/Projects.vue
@@ -1,33 +1,31 @@
 <template>
   <div class="container">
-    <b-jumbotron >
+    <b-jumbotron>
       <h1>Some projects i've done</h1>
       <hr />
       <div class="row">
-
-      <!-- project card -->
-      <b-card
-        v-for="(project, index) in projects"
-        :key="index"
-        :title="project.title"
-        :img-src="project.image"
-        :img-alt="project.alt"
-        img-top
-        tag="article"
-        style="max-width: 20rem;"
-        class="mb-2"
-      >
-      
-        <b-card-text>
-          {{project.description}}
-        </b-card-text>
-        <div class="col text-center">
-          <a :href="project.link" target="_blank" class="btn btn-secondary">Go</a>
-        </div>
-      </b-card>
-
+        <!-- project card -->
+        <b-card
+          v-for="(project, index) in projects"
+          :key="index"
+          :title="project.title"
+          :img-src="project.image"
+          :img-alt="project.alt"
+          img-top
+          tag="article"
+          style="max-width: 20rem"
+          class="mb-2"
+        >
+          <b-card-text>
+            {{ project.description }}
+          </b-card-text>
+          <div class="col text-center">
+            <a :href="project.link" target="_blank" class="btn btn-secondary"
+              >Go</a
+            >
+          </div>
+        </b-card>
       </div>
-
     </b-jumbotron>
 
     <b-card
@@ -36,7 +34,7 @@
       img-alt="Codepen image"
       img-top
       tag="article"
-      style="max-width: 18rem;"
+      style="max-width: 18rem"
       class="mb-2 container"
     >
       <b-card-text> </b-card-text>
@@ -54,33 +52,29 @@
 
 <script>
 export default {
-
-  data: function(){
-
-    return{
-      projects:{
-        1 :{
+  data: function () {
+    return {
+      projects: {
+        1: {
           title: "Personal website",
           description: "Website using Nuxt.js",
           image: "https://i.imgur.com/Rx6JWwv.png",
           link: "https://nvillabona.netlify.app/",
-          alt: "Personal website preview"
-
-
+          alt: "Personal website preview",
         },
         2: {
           title: "Platzi video",
           description: "Prototype with html & css",
           image: "https://i.imgur.com/zGvrCg3.png",
           link: "https://4szni.csb.app/",
-          alt: "Platzi video preview"
+          alt: "Platzi video preview",
         },
         3: {
           title: "Quiz App",
           description: "A quiz app built in VueJs",
           image: "https://i.imgur.com/D5ntdG6.png",
           link: "https://quiz-app-nvillabona.netlify.app/",
-          alt: "Quiz App preview"
+          alt: "Quiz App preview",
         },
         4: {
           title: "Social media dashboard",
@@ -88,26 +82,30 @@ export default {
           image: "https://i.imgur.com/x14jrEu.png",
           link: "https://social-dashboard-nvillabona.netlify.app/",
         },
-        
+        5: {
+          title: "Crypto Currency info",
+          description: "An app for getting cripto-currencies info",
+          image: "https://i.imgur.com/rnkTLXR.png",
+          link: "https://inspiring-goodall-1e0f94.netlify.app",
+        },
+
         // 3: {
         //   title: "",
         //   description: "",
         //   image: "",
         //   link: "",
         // },
-        
-      }
-    }
-  }
+      },
+    };
+  },
 };
 </script>
 
 <style>
-.card{
+.card {
   margin: auto;
-
 }
-.card-img{
+.card-img {
   border-bottom: 10px blue;
 }
 </style>
